0bc419fec8ba52e237884342c2f662968f805c95
DinhDucNguyen <ducdinh127855@gmail.com>
2024-10-12 19:24:50 +0700

    Nguyen Dinh Duc 01

--- FinalProject_Test/app/src/main/java/com/example/finalproject_test/MainScreen.java
+++ FinalProject_Test/app/src/main/java/com/example/finalproject_test/MainScreen.java
@@ -1,6 +1,10 @@
 package com.example.finalproject_test;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,60 @@ public class MainScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button play = findViewById(R.id.btnChoiNgay);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent (MainScreen.this, RandomQuiz.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        Button TheThao = findViewById(R.id.TheThao);
+        TheThao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainScreen.this, ExampleForQuizStart.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        Button khoaHoc = findViewById(R.id.KhoaHoc);
+        khoaHoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainScreen.this, ExampleQuizPlayWithTopic.class);
+                startActivity(it);
+                finish();
+            }
+        });
+      Button vanHoc = findViewById(R.id.VanHoc);
+      vanHoc.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent it = new Intent(MainScreen.this, ExampleQuizPlayWithTopic.class);
+              startActivity(it);
+              finish();
+          }
+      });
+        Button lichSu = findViewById(R.id.LichSu);
+        lichSu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainScreen.this, ExampleQuizPlayWithTopic.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        ImageButton btnUser = findViewById(R.id.User);
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainScreen.this, AccountProfile.class);
+                startActivity(it);
+                finish();
+            }
         });
     }
 }
